0edc11890f1b0cda1a7d770fd83d40026075f1e8
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-02-06 20:01:40 -0500

    Initial Convert Class commit.

--- converter.rb
+++ converter.rb
@@ -1,3 +1,14 @@
-class Converter
 
+class Converter
+  def initialize({code1 => rate1, code2 => rate2})
+    @code1 = code1
+    @rate1 = rate1
+    @code2 = code2
+    @rate2 = rate2
+  end
 end
+
+
+#don't create a hash that contains static exchange rates.
+#Instead, the rates should be passed in as parameters
+#for creating a new object in the class.
--- test.rb
+++ test.rb
@@ -1,15 +1,27 @@
 require './currency.rb'
 
 
-geoffs_money = Currency.new("$", "2.00")
-dads_money = Currency.new("$", "2.00")
+geoffs_money = Currency.new("$1000")
+dads_money = Currency.new(2.00, :USD)
 #moms_money = Currency.new("JYN", "4.00")
 
 puts "#{geoffs_money.codify} #{geoffs_money.quantify}"
 
+puts geoffs_money
+
 # puts geoffs_money == dads_money #true
 #
-# puts geoffs_money + dads_money #4.0
+c = geoffs_money + dads_money
+
+puts geoffs_money + dads_money #4.0
+
+puts c.class
+
+puts c
+
+puts geoffs_money * 3
+
+ # puts geoffs_money.class
 #
 # # puts geoffs_money - dads_money #0.0
 # #
